2f4a42992854f2304909d94e693fb4a225c3ba15
Enoch Kambale <campnoch@gmail.com>
2024-01-16 22:11:15 +0300

    first commit

---
+++ dataTypesAndVariables.lua
@@ -0,0 +1,10 @@
+--[[
+    nil
+    number 1, 1.24, 0.1, 44
+    string 'hello' "hello world"
+    boolean true false
+    table
+]]
+
+local a = 2
+print(a + 5)
---
+++ outputAndComments.lua
@@ -0,0 +1,7 @@
+print("Hello World!") -- prints text to the screen
+
+--[[
+    This program
+    is a basic  program
+    to display some text on the screen
+]]
