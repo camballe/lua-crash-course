0ee1dd67e079b6cf1495a15f7192bf4fe21f235a
Enoch Kambale <campnoch@gmail.com>
2024-01-16 22:27:47 +0300

    data types and variables

--- dataTypesAndVariables.lua
+++ dataTypesAndVariables.lua
@@ -6,5 +6,28 @@
     table
 ]]
 
-local a = 2
-print(a + 5)
+local name = "Jack"
+local surname = "Smitter"
+print("My name is " ..
+    name ..
+    ". I am 12 years old. I have decided that my name should not be " .. name .. " because the name " .. name .. " sucks")
+
+print(name .. " " .. surname)
+
+local description = [[Colo
+    Hello World
+please
+]]
+
+print(description)
+
+local old = true
+
+C = 20       -- global variable
+local c = 10 -- local variable
+print(C, c)
+
+_G.Hello = "hello" -- more recommended way of defining global variables
+
+x = "hi"
+print(type(x))
