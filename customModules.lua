2b7b007c4febbd107836334d1a345be79b581257
Enoch Kambale <campnoch@gmail.com>
2024-01-17 19:25:02 +0300

    custom modules

---
+++ customModules.lua
@@ -0,0 +1,4 @@
+local mod = require("myMath")
+
+print(mod.add(5, 10))
+print(mod.power(2, 5))
---
+++ myMath.lua
@@ -0,0 +1,11 @@
+_G.my_math = {}
+
+function my_math.add(x, y) -- we cannot use local here
+    return x + y
+end
+
+function my_math.power(num1, num2)
+    return num1 ^ num2
+end
+
+return my_math -- this can be local or global, but we need it global to use it in other files
